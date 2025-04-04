1aeb5f92f96fc08568bb17d47e581243b6bfb17d
smRid <omorsk911@gmail.com>
2025-04-04 09:53:39 +0600

    HeroSection part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
   )
 }

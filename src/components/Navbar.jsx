e8fa93670ec922a74e700f7bc1a88c29b7298844
smRid <omorsk911@gmail.com>
2025-04-04 08:55:19 +0600

    setup code update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = (
+) => {
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+    <div className="conatiner px-4 mx-auto relative text-sm">
+        <div className="flex justify-center items-center">
+            <div className="flex items-center flex-shrink-0">
+                
+            </div>
+        </div>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar

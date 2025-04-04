f6bd9e6503d62a68c92b23beb971ea2d97a72398
smRid <omorsk911@gmail.com>
2025-04-04 09:58:01 +0600

    FeatureSection part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <FeatureSection />
       </div>
     </>
   )
